f2a042329ee58d652a4a6aa51d8601ece051f6c4
MartinTeoZhang <752876237@qq.com>
2023-03-02 00:42:22 +0800

    update video picker

--- src/components/VideoPicker/index.js
+++ src/components/VideoPicker/index.js
@@ -1,14 +1,18 @@
 
+import { Col, Row, Image } from "antd"
+import './index.scss'
+import pic from '../../images/1677675297881.jpg'
+
 const items = [{
   vid: '001',
   title: '30アルファードでPATROL  東京タワー・銀座〜日本橋 【仕事グルマで広角撮影テスト】2023.01.10 TOKYO Drive',
-  image: '../../../images/1677675297881.jpg',
+  image: pic,
   duration: '01:30:25',
   url: '',
 }, {
   vid: '002',
   title: '30アルファードでPATROL  東京タワー・銀座〜日本橋 【仕事グルマで広角撮影テスト】2023.01.10 TOKYO Drive',
-  image: '../../../images/1677675297881.jpg',
+  image: pic,
   duration: '01:30:25',
   url: '',
 }]
@@ -17,7 +21,21 @@ const VideoPicker = () => {
   return (
     <div className="container">
       {items.map(item =>
-        <div className="video-box">
+        <div key={item.vid} className="video-box">
+          <Row>
+            <Col>
+              <Row><Image src="url" height={"100"}></Image></Row>
+            </Col>
+            <Col>
+              <Row>
+                <span>{item.title}</span>
+              </Row>
+              <Row>
+                <span>{item.duration}</span>
+              </Row>
+
+            </Col>
+          </Row>
           <div className="image-box">
           </div>
           <div className="info-box">
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import './index.scss'
-import { Layout } from 'antd'
+import { Layout, Col, Row } from 'antd'
 import HeadBar from '../../components/HeadBar'
-
+import VideoPicker from '../../components/VideoPicker'
 const { Header, Footer, Sider, Content } = Layout
 
 //home组件
@@ -9,7 +9,23 @@ const Home = () => {
   return (
     <div className="container">
       <Header className='header'><HeadBar /></Header>
-      <Content></Content>
+      <Content>
+        <Row align={"center"}>
+          <Col md={"24"} lg={"22"} xl={"22"}>
+            <Row style={{ background: "white", height: "100vh" }} align={"center"}>
+              <Col md={"4"} lg={"4"} xl={"4"}>
+                <VideoPicker />
+              </Col>
+              <Col md={"16"} lg={"16"} xl={"16"}>
+                <br />
+              </Col>
+              <Col md={"4"} lg={"4"} xl={"4"}>
+                <br />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
       <Footer></Footer>
 
     </div>
